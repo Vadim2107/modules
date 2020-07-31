0b247de45c3c23bf4d78d70ef33d59efcd65de13
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-07-31 18:27:51 +0300

    second commit modules

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,24 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-    // mode: 'production',
-    mode: 'development',    
+    mode: 'production',
+    // mode: 'development',    
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js.'
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             filename: "index.html"
-        })
-    ],
-    plugins: [
+        }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-    ],
+    ],        
+    
     module: {
         rules: [
             {
